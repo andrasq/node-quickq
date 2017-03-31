43633a979881b1e057aebe6bf335e1c71df07fa9
Andras <andras@kinvey.com>
2017-03-31 16:24:32 -0400

    revise benchmarks, use newer qtimeit for a visual comparison

--- benchmark.js
+++ benchmark.js
@@ -18,12 +18,21 @@ function handler(payload, cb) {
     ncalls += 1;
     // async.queue crashes if cb is called directly (max return stack size exceeded)
     // however, direct cb() is 2x faster (and quickq allows it)
-    // fastq also crashes but does not print a diagnostic!
+    // fastq also crashes but does not print a diagnostic! (does print with node-v6.7.0)
+    // wrapping cb with async.ensureAsync() fixes the crash, but is 20% slower
+
+    // fastq runs 50% faster with nextTick than setImmediate, async.queue runs 5% faster
     process.nextTick(cb);
+    //setImmediate(cb);
 }
 function handlerCb(payload, cb) {
     ncalls += 1;
     cb();
+}
+function handlerCb10(payload, cb) {
+    ncalls += 1;
+    if (ncalls % 10 === 0) setImmediate(cb);
+    else cb();
 }
 function taskDone() {
     ndone += 1;
@@ -36,32 +45,40 @@ aflow.repeatWhile(
         return iteration-- > 0;
     },
     function(cb) {
-        var ntasks = 100000;
-        var q1 = async.queue(handler, 10);
-        var q2 = fastq(handler, 10);
-        var q3 = quickq(handlerCb, {concurrency: 10});
-        qtimeit.bench.timeGoal = 2;
+        var ntasks = 10000;
+        var concurrency = 10;
+
+        qtimeit.bench.timeGoal = 1;
         qtimeit.bench.opsPerTest = ntasks;
+        qtimeit.bench.visualize = true;
         qtimeit.bench({
             'async.queue': function(done) {
                 ncalls = ndone = 0;
-                var q = async.queue(handler, 10);
+                var q = async.queue(handler, concurrency);                // 205k/s
+                //var q = async.queue(async.ensureAsync(handlerCb), 10);  // 162k/s
+                //var q = async.queue(async.ensureAsync(handler), 10);    // 171-177k/s
+                //var q = async.queue(handlerCb10, 10);                   // 221k/s
                 q.drain = done;
                 for (var i=0; i<ntasks; i++) q.push(0, taskDone);
             },
+
+            // fastq is extremely slow under node-v0.10.42
             fastq: function(done) {
                 ncalls = ndone = 0;
-                var q = fastq(handler, 10);
+                var q = fastq(handler, concurrency);
                 q.drain = done;
                 for (var i=0; i<ntasks; i++) q.push(0, taskDone);
             },
+
             // Note: running quickq in combination with fastq will cause quickq
             // throughput to drop 45% on the 5th-6th run, from 5.0m/s to 2.7m/s
             // (ie, from 6x faster to 3x faster than fastq).
             // Quickq by itself or with just async.queue stays fast.
+            // Same on Skylake, w/ 10k tasks 5th run drops from 17m/s to 10m/s with setImmediate
+            // or 18m/s to 13m/s on 7th run with nextTick (async.queue drops from 860k/s to 740k/s)
             quickq: function(done) {
                 ncalls = ndone = 0;
-                var q = quickq(handlerCb, {concurrency: 10});
+                var q = quickq(handlerCb, {concurrency: concurrency});
                 q.drain = done;
                 for (var i=0; i<ntasks; i++) q.push(0, taskDone);
             },
